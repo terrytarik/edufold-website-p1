1750c0561cbbceba8f81780a4ab12ba33b6167cf
terrytarik <taraskavkalo@gmail.com>
2022-06-15 16:52:46 +0300

    add first page

---
+++ menu_logic.js
@@ -0,0 +1,27 @@
+
+let navLinks = document.getElementById("navLinks");
+let nav = document.getElementsByTagName("nav")[0];
+let logo = document.getElementsByClassName("fa-graduation-cap")[0];
+
+let flag = false;
+addEventListener("scroll", () => {
+  if(!flag) {
+    nav.style.backgroundColor = "#f44336"
+    logo.style.color = "#fff"
+    flag = true;
+  }  
+
+  if(scrollY == 0) {
+    flag = false;
+    nav.style.backgroundColor = "transparent";
+    logo.style.color = "#f44336"
+  }
+})
+
+function showMenu () {
+  navLinks.style.right = "0";
+}
+
+function hideMenu () {
+  navLinks.style.right = "-150px";
+}
